a36e3d4f11da4cb2fdc550eccd31eb38a4986b9b
miguel0920 <miguel.puente@upb.edu.co>
2026-05-19 15:09:31 -0500

    Crear Interfaces

---
+++ INotificador.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalPOO
+{
+    public interface INotificador
+    {
+        void Notificar(string mensaje);
+    }
+}
--- Pedido.cs
+++ Pedido.cs
@@ -4,22 +4,19 @@ using System.Text;
 
 namespace ProyectoFinalPOO
 {
-    namespace ProyectoFinalPOO
+    public class Producto
     {
-        public class Producto
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int Stock { get; set; }
+
+        public Producto(string nombre, decimal precio, int stock)
         {
-            public string Nombre { get; set; }
-
-            public decimal Precio { get; set; }
-
-            public int Stock { get; set; }
-
-            public Producto(string nombre, decimal precio, int stock)
-            {
-                Nombre = nombre;
-                Precio = precio;
-                Stock = stock;
-            }
+            Nombre = nombre;
+            Precio = precio;
+            Stock = stock;
         }
     }
 }
